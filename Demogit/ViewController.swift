b7842d89fb3e49ef29c89d387817588fbd1fd219
Copy Liao <Copy.Liao@dlinkcorp.com>
2018-08-02 11:28:19 +0800

    master2

--- Demogit/ViewController.swift
+++ Demogit/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("wdw")
         print("master111")
+        print("master222")
+        print("master222")
     }
 
     override func didReceiveMemoryWarning() {
